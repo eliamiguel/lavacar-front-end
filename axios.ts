2395bb6a5148151ededde83f981c6a38bf8009ff
eliamiguel <eliasperreiramiguel@gmail.com>
2025-03-07 14:04:45 -0300

    ajustando os arquios axios e next.config para ficar igual a de produção

--- axios.ts
+++ axios.ts
@@ -1,5 +1,21 @@
 import axios from 'axios';
+
 export const makeRequest = axios.create({
-  baseURL: "http://localhost:8002", 
-  withCredentials: true
+  baseURL: process.env.NODE_ENV === 'production'
+    ? "https://lavacar.gestaobc.net.br/api"
+    : "http://localhost:8003/api",  // Para desenvolvimento
+  withCredentials: true,
 })
+
+
+
+  makeRequest.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem("lavacar:token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
--- next.config.ts
+++ next.config.ts
@@ -37,7 +37,7 @@ const nextConfig: NextConfig = {
     ],
   },
   productionBrowserSourceMaps: false,
-  webpack(config) {
+  webpack(config:any) {
     
     return config;
   },
